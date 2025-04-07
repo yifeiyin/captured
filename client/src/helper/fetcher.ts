c1f18f97b60b166c0d20958f9be50e8d58a05f43
Yifei Yin <contact@yyin.me>
2025-04-07 18:45:18 -0400

    Resolve deprecated API

--- client/src/helper/fetcher.ts
+++ client/src/helper/fetcher.ts
@@ -1,10 +1,10 @@
-import { type CreateTRPCClient, createTRPCClient, httpBatchLink } from '@trpc/client';
+import { type TRPCClient, createTRPCClient, httpBatchLink } from '@trpc/client';
 // @ts-nocheck
 import type { AppRouter } from '../../../server/src/handlers';
 
 // Pass AppRouter as generic here. 👇 This lets the `trpc` object know
 // what procedures are available on the server and their input/output types.
-export const trpc: CreateTRPCClient<AppRouter> = createTRPCClient<AppRouter>({
+export const trpc: TRPCClient<AppRouter> = createTRPCClient<AppRouter>({
   links: [
     httpBatchLink({
       url: `${import.meta.env.VITE_BACKEND_URL}/api/`,
